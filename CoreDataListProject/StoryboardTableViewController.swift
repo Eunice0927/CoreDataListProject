bb9f78fcaae4893dffd92dff81f199cf7ef66408
Eunice <jes.e.0927@gmail.com>
2023-11-23 17:49:49 +0900

    ✨ Feat: Change View which is generated with storyboard
    To show more data in the cell, changed previous view(CoreDataViewController) to storyboard based view(StoryBoardTableView)

--- CoreDataListProject/StoryboardTableViewController.swift
+++ CoreDataListProject/StoryboardTableViewController.swift
@@ -9,15 +9,12 @@ import UIKit
 
 class StoryboardTableViewController: UITableViewController {
     let coreDataManager = CoreDataManager()
+    @IBOutlet var storyboardTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        coreDataManager.tableView = storyboardTableView
+        self.tableView.rowHeight = 60
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -29,68 +26,18 @@ class StoryboardTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return coreDataManager.models.count
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let model = coreDataManager.models[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-//        cell.textLabel?.text = "\(model.name) - \(model.createdAt)"
-        cell.textLabel?.text = model.name
+        let cell = tableView.dequeueReusableCell(withIdentifier: "storyboardCell", for: indexPath) as! StoryboardTableViewCell
+        let date = model.createdAt?.formatted(date: .numeric, time: .standard)
+        cell.rank.text = "\(indexPath[1] + 1)"
+        cell.record.text = model.name
+        cell.time.text = "\(date ?? "No time Data")"
+        
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
