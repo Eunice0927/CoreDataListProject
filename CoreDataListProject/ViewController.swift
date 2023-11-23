47be593c6ce87653be62ec84315d7721b0f49017
Eunice <jes.e.0927@gmail.com>
2023-11-23 13:39:42 +0900

    ✨ Feat: Get, create, delete, update data
    - getAllItems(): Get all data from db
    - createItem(): Add new item
    - deleteItem(): Delete item
    - updateItem(): Update item name

--- CoreDataListProject/ViewController.swift
+++ CoreDataListProject/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // where to perform objects in the coredata db
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // call all items of db
+    func getAllItems() {
+        do {
+            let items = try context.fetch(ToDoListItem.fetchRequest()) //subclass 추가하며 자동생성되었다.
+        } catch let error {
+            //error
+            print(error)
+        }
+    }
+    
+    func createItem(name: String) {
+        let newItem = ToDoListItem(context: context)
+        newItem.name = name
+        newItem.createdAt = Date()
+        
+        do {
+            try context.save()
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func deleteItem(item: ToDoListItem) {
+        context.delete(item)
+        
+        do {
+            try context.save()
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func updateItem(item: ToDoListItem, newName: String) {
+        item.name = newName
+        
+        do {
+            try context.save()
+        } catch let error {
+            print(error)
+        }
+    }
 }
 
